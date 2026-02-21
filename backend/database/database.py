f13add63437fbd81766d60439153b90215868dca
RIKESHWARAN M <rikeshrikey@gmail.com>
2026-02-21 10:29:58 +0530

    db

--- backend/database/database.py
+++ backend/database/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
-DATABASE_URL = "mysql+mysqlconnector://root:maha@localhost/store_rating_db"
+DATABASE_URL = "mysql+mysqlconnector://root:YourDBPassword@localhost/store_rating_db"
 
 engine = create_engine(DATABASE_URL)
 
